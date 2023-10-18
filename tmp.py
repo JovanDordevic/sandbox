71b4989e5f9d6fbae549006f693589da32bc3b74
Jovan Djordjevic <jovan.dordevic@energysage.com>
2023-10-18 10:38:41 +0200

    tmp6

--- tmp.py
+++ tmp.py
@@ -3,7 +3,7 @@ import os
 print("Hello from tmp.py")
 print()
 # Returns `default_value` if the key doesn't exist
-ENV_NUM = os.getenv('ENV_NUM', 'it does not exist!')
+ENV_NUM = int(os.getenv('ENV_NUM', -1))
 print(f"ENV_NUM = {ENV_NUM}")
 print()
 print(f"whole env:\n {os.environ}")
